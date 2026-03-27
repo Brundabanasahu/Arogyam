8797227a550b8eac0671c92a00925fb8f54a3ee1
Brundabana Sahu <sbrundabana12@gmail.com>
2026-03-27 12:03:28 +0530

    final backend fix

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,28 @@ const healthRoutes = require("./routes/healthRoutes");
 const auth = require("./middleware/auth");
 
 const app = express();
-const PORT = 5000;
-const MONGO_URL = "mongodb://127.0.0.1:27017/elderDB";
 
+// ✅ IMPORTANT
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 app.use(cors());
 app.use(express.json());
 
+// DB CONNECT
 mongoose
   .connect(MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("MongoDB connection error:", error.message));
 
+// ROUTES
 app.use("/auth", authRoutes);
 app.use("/api", auth, healthRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend running...");
+});
+
+// START SERVER
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
